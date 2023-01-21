349807ca7d7640e6c248cd186124ee89ac79c546
Raj Dutta <drajarshi01@gmail.com>
2023-01-21 23:00:19 +0530

    edit collectstatic

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "Start"
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "Done"
